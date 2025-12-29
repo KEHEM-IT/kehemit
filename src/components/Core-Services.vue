ff406149f368c96b3a1d17891e330b01823d5f33
Surajit Singha Sisir <sci.surajit@gmail.com>
2025-12-30 02:23:59 +0600

    core working-1
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- src/components/Core-Services.vue
+++ src/components/Core-Services.vue
@@ -1,8 +1,12 @@
 <!-- src/components/Core-Services.vue -->
 <template>
-    <section>
-        <div class="heading">
+    <section class="core-services">
+        <div class="heading-block">
+            <span class="heading-back">Services</span>
             <h1>Core Services</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio illum numquam assumenda molestias
+                ipsam ratione earum omnis? Sapiente consectetur ea, necessitatibus optio natus perspiciatis molestiae
+                deleniti commodi repudiandae vitae iure. </p>
         </div>
     </section>
 </template>
